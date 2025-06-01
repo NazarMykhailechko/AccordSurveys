41adae9e60bfe74a287dbad3cd1c00a908f9ac44
unknown <win72007@ukr.net>
2025-06-01 16:40:27 +0300

    added buildpack odbc

---
+++ app111.R
@@ -0,0 +1,34 @@
+library(shiny)
+library(DT)
+library(data.table)
+library(reactable)
+library(dplyr)
+library(RODBC)
+
+ui <- page_fluid(
+  tableOutput("table") 
+)
+
+server <- function(input, output) {
+  
+  
+  ssql <- "SELECT * FROM INFORMATION_SCHEMA.TABLES"
+  #where login = 'olanhaieva'"
+  
+  #con <- RODBC::odbcDriverConnect('driver={SQL Server};server=localhost\\SQLEXPRESS;database=crmsystem;trusted_connection=true')
+  #con <- RODBC::odbcDriverConnect('driver={SQL Server};Server=tcp:ne-az-sql-serv1.database.windows.net;database=daqk9mfw8cyjjxu;uid=upe7l0su0vij18r;pwd=wQxX6Z85n0DJx*Y#i5QFYS4lD;')
+  con <- RODBC::odbcDriverConnect('driver={SQL Server};Server=tcp:ne-az-sql-serv1.database.windows.net;database=dl1xlqmw32xedun;uid=us1nj4boyvsubkt;pwd=oi#WOBSAHjqk4SkqXC?rsYyA$;')
+  #data <- RODBC::sqlQuery(con, "SELECT * FROM mycredentials")
+  #on.exit(RODBC::odbcClose(con))
+  
+  #con <- RODBC::odbcDriverConnect('driver={SQL Server};server=DESKTOP-3D58FB2\\SQLEXPRESS;database=crmsystem;trusted_connection=true')
+  data <- RODBC::sqlQuery(con, ssql)
+  
+  
+  on.exit(RODBC::odbcClose(con))
+  
+  output$table <- 
+    renderTable(data)
+}
+
+shinyApp(ui = ui, server = server)
