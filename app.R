dc250f35cb6007a48cf9780b55d6cf1eae61e0ff
unknown <win72007@ukr.net>
2025-06-01 14:34:21 +0300

    added buildpack odbc

--- app.R
+++ app.R
@@ -6,40 +6,29 @@ library(dplyr)
 library(RODBC)
 
 ui <- page_fluid(
-  reactableOutput("table") 
+  tableOutput("table") 
 )
 
 server <- function(input, output) {
   
-  data <- reactive({
-    
-    
-    #print(auth$user)
-    
-    
-    ssql <- "SELECT * FROM INFORMATION_SCHEMA.TABLES"
-    #where login = 'olanhaieva'"
-    
-    #con <- RODBC::odbcDriverConnect('driver={SQL Server};server=localhost\\SQLEXPRESS;database=crmsystem;trusted_connection=true')
-    #con <- RODBC::odbcDriverConnect('driver={SQL Server};Server=tcp:ne-az-sql-serv1.database.windows.net;database=daqk9mfw8cyjjxu;uid=upe7l0su0vij18r;pwd=wQxX6Z85n0DJx*Y#i5QFYS4lD;')
-    con <- RODBC::odbcDriverConnect('driver={SQL Server};Server=tcp:ne-az-sql-serv1.database.windows.net;database=dl1xlqmw32xedun;uid=us1nj4boyvsubkt;pwd=oi#WOBSAHjqk4SkqXC?rsYyA$;')
-    #data <- RODBC::sqlQuery(con, "SELECT * FROM mycredentials")
-    #on.exit(RODBC::odbcClose(con))
-    
-    #con <- RODBC::odbcDriverConnect('driver={SQL Server};server=DESKTOP-3D58FB2\\SQLEXPRESS;database=crmsystem;trusted_connection=true')
-    data <- RODBC::sqlQuery(con, ssql)
-    
-    
-    on.exit(RODBC::odbcClose(con))
-    
-    
-    return(data)
-    
-  })
   
+  ssql <- "SELECT * FROM INFORMATION_SCHEMA.TABLES"
+  #where login = 'olanhaieva'"
+  
+  #con <- RODBC::odbcDriverConnect('driver={SQL Server};server=localhost\\SQLEXPRESS;database=crmsystem;trusted_connection=true')
+  #con <- RODBC::odbcDriverConnect('driver={SQL Server};Server=tcp:ne-az-sql-serv1.database.windows.net;database=daqk9mfw8cyjjxu;uid=upe7l0su0vij18r;pwd=wQxX6Z85n0DJx*Y#i5QFYS4lD;')
+  con <- RODBC::odbcDriverConnect('driver={SQL Server};Server=tcp:ne-az-sql-serv1.database.windows.net;database=dl1xlqmw32xedun;uid=us1nj4boyvsubkt;pwd=oi#WOBSAHjqk4SkqXC?rsYyA$;')
+  #data <- RODBC::sqlQuery(con, "SELECT * FROM mycredentials")
+  #on.exit(RODBC::odbcClose(con))
+  
+  #con <- RODBC::odbcDriverConnect('driver={SQL Server};server=DESKTOP-3D58FB2\\SQLEXPRESS;database=crmsystem;trusted_connection=true')
+  data <- RODBC::sqlQuery(con, ssql)
+  
+  
+  on.exit(RODBC::odbcClose(con))
   
   output$table <- 
-    renderReactable({ reactable(data())}) 
+    renderTable(data)
 }
 
 shinyApp(ui = ui, server = server)
