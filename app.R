df226e69f0bec1b312e3076b227f17441ac177ca
unknown <win72007@ukr.net>
2025-06-01 17:33:19 +0300

    added buildpack odbc

--- app.R
+++ app.R
@@ -3,8 +3,7 @@ library(DT)
 library(data.table)
 library(reactable)
 library(dplyr)
-library(DBI)
-library(odbc)
+library(RODBC)
 
 
 
@@ -25,25 +24,17 @@ server <- function(input, output) {
     
     #con <- RODBC::odbcDriverConnect('driver={SQL Server};server=localhost\\SQLEXPRESS;database=crmsystem;trusted_connection=true')
     #con <- RODBC::odbcDriverConnect('driver={SQL Server};Server=tcp:ne-az-sql-serv1.database.windows.net;database=daqk9mfw8cyjjxu;uid=upe7l0su0vij18r;pwd=wQxX6Z85n0DJx*Y#i5QFYS4lD;')
-    #con <- RODBC::odbcDriverConnect('driver={SQL Server};Server=tcp:ne-az-sql-serv1.database.windows.net;database=dl1xlqmw32xedun;uid=us1nj4boyvsubkt;pwd=oi#WOBSAHjqk4SkqXC?rsYyA$;')
+    con <- RODBC::odbcDriverConnect('driver={SQL Server};Server=tcp:ne-az-sql-serv1.database.windows.net;database=dl1xlqmw32xedun;uid=us1nj4boyvsubkt;pwd=oi#WOBSAHjqk4SkqXC?rsYyA$;')
     #con <- odbcConnect("ne-az-sql-serv1.database.windows.net", uid="us1nj4boyvsubkt", pwd="oi#WOBSAHjqk4SkqXC?rsYyA$")
     #data <- RODBC::sqlQuery(con, "SELECT * FROM mycredentials")
     #on.exit(RODBC::odbcClose(con))
     
     #con <- RODBC::odbcDriverConnect('driver={SQL Server};server=DESKTOP-3D58FB2\\SQLEXPRESS;database=crmsystem;trusted_connection=true')
-    #data <- RODBC::sqlQuery(con, ssql)
-    
-    conn <- dbConnect(odbc::odbc(), 
-                      Driver = "SQL Server", 
-                      Server = "ne-az-sql-serv1.database.windows.net", 
-                      Database = "dl1xlqmw32xedun", 
-                      UID = "us1nj4boyvsubkt", 
-                      PWD = "oi#WOBSAHjqk4SkqXC?rsYyA$")
-    
-    data <- dbGetQuery(conn, ssql)
+    data <- RODBC::sqlQuery(con, ssql)
+  
     
     
-    #on.exit(RODBC::odbcClose(con))
+    on.exit(RODBC::odbcClose(con))
     
     
     return(data)
