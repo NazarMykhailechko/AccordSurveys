a2b773afbea647365f51d235a4b620c7a72b5698
unknown <win72007@ukr.net>
2025-06-01 21:55:26 +0300

    added buildpack odbc

--- app.R
+++ app.R
@@ -3,7 +3,9 @@ library(DT)
 library(data.table)
 library(reactable)
 library(dplyr)
-library(RODBC)
+#library(RODBC)
+library(DBI)
+library(odbc)
 
 ui <- shiny::fluidPage(
   tableOutput("table") 
@@ -21,17 +23,28 @@ server <- function(input, output) {
     #where login = 'olanhaieva'"
     
     #con <- RODBC::odbcDriverConnect('driver={SQL Server};server=localhost\\SQLEXPRESS;database=crmsystem;trusted_connection=true')
-    con <- RODBC::odbcDriverConnect('driver={ODBC Driver 17 for SQL Server};Server=tcp:ne-az-sql-serv1.database.windows.net;database=dskzhj3orbdvtav;uid=ull5ks6f0yft2ip;pwd=JAmp1yCnhCcl#IcdykLOq8Psj;')
+    #con <- RODBC::odbcDriverConnect('driver={SQL Server};Server=tcp:ne-az-sql-serv1.database.windows.net;database=dskzhj3orbdvtav;uid=ull5ks6f0yft2ip;pwd=JAmp1yCnhCcl#IcdykLOq8Psj;')
     #con <- RODBC::odbcDriverConnect('driver={SQL Server};Server=tcp:ne-az-sql-serv1.database.windows.net;database=dl1xlqmw32xedun;uid=us1nj4boyvsubkt;pwd=oi#WOBSAHjqk4SkqXC?rsYyA$;')
     #data <- RODBC::sqlQuery(con, "SELECT * FROM mycredentials")
     #on.exit(RODBC::odbcClose(con))
     
     #con <- RODBC::odbcDriverConnect('driver={ODBC Driver 17 for SQL Server};server=DESKTOP-3D58FB2\\SQLEXPRESS;database=crmsystem;trusted_connection=true')
     #con <- RODBC::odbcDriverConnect('driver={SQL Server};server=DESKTOP-3D58FB2\\SQLEXPRESS;database=crmsystem;trusted_connection=true')
-    data <- RODBC::sqlQuery(con, ssql)
+    #data <- RODBC::sqlQuery(con, ssql)
     
+    con <- dbConnect(odbc(),
+                     Driver = "ODBC Driver 17 for SQL Server",
+                     Server = "ne-az-sql-serv1.database.windows.net",
+                     Database = "dskzhj3orbdvtav",
+                     UID = "ull5ks6f0yft2ip",
+                     PWD = "JAmp1yCnhCcl#IcdykLOq8Psj",
+                     Port = 1433)
     
-    on.exit(RODBC::odbcClose(con))
+    data <- dbGetQuery(con, ssql)
+    
+    #data <- RODBC::sqlQuery(con, ssql)
+    
+    #on.exit(RODBC::odbcClose(con))
     
     
     return(data)
